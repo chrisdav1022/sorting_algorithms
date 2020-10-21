9027bd872a105de51f525171ca1e9e36ce1237c5
chrisdav1022 <chris_bedoya01@hotmail.com>
2020-10-21 20:51:00 +0000

    current file

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,25 +9,24 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i;
-	int j, t;
+	unsigned int j = 0;
+	int t;
 
-	if (!array || !size)
+	if (!array || !size || size < 2)
 		return;
 
-	do 
+	while (j < size)
 	{
-		j = 0;
 		for (i = 0; i < size - 1; i++)
 		{
 			if (array[i] > array[i + 1])
 			{
-				t = array[i + 1];
-				array[i + 1] = array[i];
-				array[i] = t;
-				j = 1;
+				t = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = t;
 				print_array(array, size);
 			}
 		}
-	} 
-	while (j);
+		j++;
+	}
 }

c251c6c1588c8f33436793071633ad6188c9a93b
chrisdav1022 <chris_bedoya01@hotmail.com>
2020-10-21 20:20:46 +0000

    current file

---
+++ 0-bubble_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Sort integers algorithm
+ * @array: The array
+ * @size: The number
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i;
+	int j, t;
+
+	if (!array)
+		return;
+
+	do 
+	{
+		j = 0;
+		for (i = 0; i < size - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				t = array[i + 1];
+				array[i + 1] = array[i];
+				array[i] = t;
+				j = 1;
+				print_array(array, size);
+			}
+		}
+	} 
+	while (j);
+}
